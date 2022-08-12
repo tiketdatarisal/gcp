424d8238c8552beb6537afa2f627655f8eeeece4
Risal <risal.risal@tiket.com>
2022-08-12 15:32:57 +0700

    Add Bigquery module

---
+++ bigquery/vars.go
@@ -0,0 +1,25 @@
+package bigquery
+
+import (
+	"errors"
+	"sync"
+	"time"
+)
+
+const (
+	timeoutDuration = 30 * time.Second
+	errorWrapper    = "%w: %v"
+)
+
+var (
+	q     = map[string]*BigQuery{}
+	mutex sync.Mutex
+	Error error
+
+	ErrInitBigQueryClientFailed = errors.New("could not initialize BigQuery client")
+	ErrGetTableNamesFailed      = errors.New("could not get BigQuery table names")
+	ErrCreateTableFailed        = errors.New("could not create BigQuery table")
+	ErrDeleteTableFailed        = errors.New("could not delete BigQuery table")
+	ErrGetTableSchemaFailed     = errors.New("could not get BigQuery table schema")
+	ErrInsertRowFailed          = errors.New("could not insert new row to BigQuery table")
+)
