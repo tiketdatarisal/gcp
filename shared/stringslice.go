256ccc854b1006e091ac37f0ba8c5440b2ecd8fe
tiketdatarisal <risal.risal@tiket.com>
2023-01-10 11:45:13 +0700

    Update stringslice.go

--- shared/stringslice.go
+++ shared/stringslice.go
@@ -1,14 +1,13 @@
 package shared
 
 import (
-	"sort"
 	"strings"
 )
 
 type StringSlice []string
 
-func (s *StringSlice) Contains(text string) bool {
-	for _, str := range *s {
+func (s StringSlice) Contains(text string) bool {
+	for _, str := range s {
 		if str == text {
 			return true
 		}
@@ -17,11 +16,6 @@ func (s *StringSlice) Contains(text string) bool {
 	return false
 }
 
-func (s *StringSlice) String() string {
-	return strings.Join(*s, ", ")
-}
-
-func (s *StringSlice) Sort() StringSlice {
-	sort.Strings(*s)
-	return *s
+func (s StringSlice) String() string {
+	return strings.Join(s, ", ")
 }
