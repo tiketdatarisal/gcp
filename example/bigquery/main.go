9a31aad45e9baa285511136e90cdeef5024ded46
Risal <risal.risal@tiket.com>
2023-02-21 19:17:49 +0700

    Add RunQueryToCSV, RunQueryToJSON

--- example/bigquery/main.go
+++ example/bigquery/main.go
@@ -8,7 +8,7 @@ import (
 )
 
 func main() {
-	client := bigquery.Singleton("tiket-0818", `path-to-credential`)
+	client := bigquery.Singleton("tiket-0818", `path-to-credentials`)
 	if bigquery.Error != nil {
 		panic(bigquery.Error)
 	}
@@ -35,20 +35,24 @@ func main() {
 		fmt.Println()
 	}
 
-	if columns, err := client.GetColumnMetadata("galaxy_dwh", "verified_active_user"); err != nil {
+	if columns, err := client.GetColumnMetadata("galaxy_dwh", "ancillary_flight_order"); err != nil {
 		panic(err)
 	} else {
 		fmt.Println("Columns Metadata:", columns)
 		fmt.Println()
 	}
 
-	if result, err := client.RunQuery("SELECT * FROM `tiket-0818.galaxy_dwh.verified_active_user` LIMIT 10"); err != nil {
+	if result, err := client.RunQuery("SELECT * FROM `tiket-0818.galaxy_dwh.ancillary_flight_order` LIMIT 10", nil); err != nil {
 		panic(err)
 	} else {
 		fmt.Println("Query Result:", result)
 	}
 
-	if err := client.ExportToCsv("SELECT * FROM `tiket-0818.galaxy_dwh.verified_active_user` LIMIT 10", "gs://bucket_name/object_name.csv", 3, 2*time.Second); err != nil {
+	if err := client.ExportToCsv("SELECT * FROM `tiket-0818.galaxy_dwh.ancillary_flight_order` LIMIT 10", nil, "gs://data_risal/sample.csv", 3, 2*time.Second); err != nil {
+		panic(err)
+	}
+
+	if err := client.RunQueryToCSV("SELECT * FROM `tiket-0818.galaxy_dwh.ancillary_flight_order`", "gs://data_risal/exported/sample-*.csv"); err != nil {
 		panic(err)
 	}
 }
